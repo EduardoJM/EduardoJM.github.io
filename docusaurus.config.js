3997c0e43a443ced38d9094b150528cb64f7681a
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-12-27 08:58:20 -0300

    chore: change the repository configurations

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Anotações de Eduardo',
   tagline: 'Repositório de Anotações de Eduardo',
   url: 'https://eduardojm.github.io',
-  baseUrl: '/notes/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'EduardoJM', // Usually your GitHub org/user name.
-  projectName: 'notes', // Usually your repo name.
+  projectName: 'EduardoJM.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/EduardoJM/notes/tree/main/',
+            'https://github.com/EduardoJM/EduardoJM.github.io/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
