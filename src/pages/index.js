0623d6d73ed06a39ff5fff9937f537eb87476fa2
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-02-18 20:44:23 -0300

    feat: create the base of the metric component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import HomepageHeader from '@site/src/components/homepage-header';
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageMetrics from "@site/src/components/homepage-metrics";
+import HomepageHeader from "@site/src/components/homepage-header";
 
 export default function Home() {
   return (
-    <Layout
-      title="Início"
-      description="Anotações de Eduardo Oliveira">
+    <Layout title="Início" description="Anotações de Eduardo Oliveira">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <HomepageMetrics />
       </main>
     </Layout>
   );
