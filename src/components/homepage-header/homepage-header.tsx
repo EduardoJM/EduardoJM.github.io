f1b379c47a54d241042a9f32e45fa12a1461a3d3
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-02-18 20:29:31 -0300

    feat: create the basic header

--- src/components/homepage-header/homepage-header.tsx
+++ src/components/homepage-header/homepage-header.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './homepage-header.module.css';
-
+import React from "react";
+import clsx from "clsx";
+import styles from "./homepage-header.module.css";
 
 const HomepageHeader = () => {
-
-    return (
-        <header className={clsx({"hero hero--primary": true, [styles.pageHeader]: true })}>
-            <div className="container">
-                <div className={styles.content}>
-                    <div className={styles.textContent}>
-                        <h1>Eduardo Oliveira</h1>
-                        <h2>Software Engineer</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum, erat vel aliquet feugiat, neque dui rutrum risus, at mattis magna lectus at felis. Proin eu felis non orci lobortis fermentum facilisis sit amet neque. Curabitur finibus libero eu facilisis pretium. Ut facilisis tristique velit, eget rutrum nulla pretium ac.</p>
-                    </div>
-                </div>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className={styles.pageHeader}>
+      <div className="container">
+        <div className={styles.content}>
+          <div className={styles.textContent}>
+            <h2>Software Engineer</h2>
+            <h1>Eduardo Oliveira</h1>
+            <p>
+              Full Stack Software Enginner with background in academic research
+              in applied and computational mathematics.
+            </p>
+          </div>
+          <div className={styles.imageContent}>
+            <svg width="512" height="712" viewBox="0 0 512 712">
+              <ellipse
+                cx="300"
+                cy="320"
+                rx="200"
+                ry="200"
+                className={styles.firstBall}
+              />
+              <ellipse
+                cx="230"
+                cy="460"
+                rx="150"
+                ry="150"
+                className={styles.secondBall}
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default HomepageHeader;
